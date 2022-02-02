d7b2703699a091aa684e73b744ff05655d453c4c
Jonathan Lopes <63373963+jonathan-lopes@users.noreply.github.com>
2022-02-02 14:45:42 -0300

    add home page features

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,30 @@
+import { useState } from "react";
 import { FiLink } from "react-icons/fi";
+import LinkItem from "../../components/LinkItem";
 import Menu from "../../components/Menu";
+import api from "../../services/api";
 import "./styles.css";
 
 function Home() {
+  const [link, setLink] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [data, setData] = useState({});
+
+  async function handleShortLink() {
+    try {
+      const response = await api.post("/shorten", {
+        long_url: link,
+      });
+
+      setData(response.data);
+      setShowModal(true);
+      setLink("");
+    } catch (error) {
+      alert("Ops parece que algo deu errado!");
+      setLink("");
+    }
+  }
+
   return (
     <div className="container-home">
       <div className="logo">
@@ -10,16 +32,20 @@ function Home() {
         <h1>SujeitoLink</h1>
         <span>Cole seu link para encurtar 👇</span>
       </div>
-
       <div className="area-input">
         <div>
           <FiLink size={24} color="#fff" />
-          <input type="text" placeholder="Cole seu link aqui..." />
+          <input
+            type="text"
+            placeholder="Cole seu link aqui..."
+            value={link}
+            onChange={(e) => setLink(e.target.value)}
+          />
         </div>
-        <button>Encurtar Link</button>
+        <button onClick={handleShortLink}>Encurtar Link</button>
       </div>
-
       <Menu />
+      {showModal && <LinkItem setShowModal={setShowModal} data={data} />}
     </div>
   );
 }
